6a9c99ce9e1663d095b77831e1edae2568717352
ValentinaSisini <valentinasisini@gmail.com>
2022-05-23 19:39:43 +0200

    [ADD] tests: - 1 - verifies if the contract has been deployed - 2 - verifies the initial message has been set correctly -     3 - verifies the function that changes the message works correctly

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,26 +6,9 @@ const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require("../compile");
 
-// Versione con le promise, non la usiamo più
-/*
-beforeEach(() => {
-    // Get a list of all accounts
-    web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-        console.log(fetchedAccounts);
-    });
-    // Use one of those accounts to deploy the contract
-});
-*/
-
-/*
-describe('Inbox', () => {
-    it('deploys a contract', () => {});
-});
-*/
-
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -33,40 +16,28 @@ beforeEach(async () => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas:"1000000" })
 });
 
 describe("Inbox", () => {
+    // 1 - verifies if the contract has been deployed
     it("deploys a contract", () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    // 2 - verifies the initial message has been set correctly
+    it("has a default message", async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING)
+    });
+
+    // 3 - verifies the function that changes the message works correctly
+    it("can change the message", async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye')
+
     });
 });
 
-/*
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
-beforeEach(() => {
-    car = new Car();
-});
-
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    });
-});
-*/
